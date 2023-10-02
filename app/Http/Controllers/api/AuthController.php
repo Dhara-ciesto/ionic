9196a0771cd7563d9cf033724b9353b7c7339c63
Der <Der@example.com>
2023-10-02 18:26:18 +0530

    Update AuthController.php

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\api;
 
 use App\Models\OTP;
 use App\Models\User;
-use Twilio\Rest\Client;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request as Psr7Request;
 
 class AuthController extends Controller
 {
@@ -32,6 +32,7 @@ class AuthController extends Controller
             $errors = $validator->errors();
             // return response()->json(['status' => '0', 'message' => $validator->messages()->first()]);
             return response()->json([
+                'success' => false,
                 'message' => $validator->messages()->first()
             ], 200);
         }
@@ -47,6 +48,7 @@ class AuthController extends Controller
             $token = $user->createToken('auth_token')->plainTextToken;
 
             return response()->json([
+                'success' => true,
                 'access_token' => $token,
                 'token_type' => 'Bearer',
             ]);
@@ -58,75 +60,59 @@ class AuthController extends Controller
     public function sendotp(Request $request)
     {
 
-        $request->validate(['whatsapp_no' => 'required']);
+        $validator = Validator::make($request->all(), [
+            'whatsapp_no' => 'required|unique:users,whatsapp_no|digits:10'
+        ],[
+            'whatsapp_no.required' => 'The Whatsapp No is required',
+            'whatsapp_no.digits' => 'The Whatsapp No must be digit',
+            'whatsapp_no.unique' => 'The Whatsapp No is already been taken'
+        ]);
+        // Return errors if validation error occur.
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            // return response()->json(['status' => '0', 'message' => $validator->messages()->first()]);
+            return response()->json([
+                'message' => $validator->messages()->first()
+            ], 200);
+        }
+
 
         $otp = OTP::create([
             'otp' => rand(111111, 999999),
             'whatsapp_no' => $request->whatsapp_no,
         ]);
-
+        $msg = urlencode("Dear, Your OTP for registration is $otp->otp");
+        $number = $request->whatsapp_no;
+        $client = new Client();
+        $d = new Psr7Request('GET', "http://waw.vr4creativity.com/wapp/api/send?apikey=d82c7bbb589046c9aed04518f78ae45a&mobile=$number&msg=$msg");
+        $res = $client->sendAsync($d)->wait();
         return response()->json([
-            'otp' => $otp->otp,
+            // 'otp' => $otp->otp,
             'message' => 'OTP Sent'
         ]);
-        // $message = "Hii";
-        // $recipients = '+9173158283';
-        // $account_sid = getenv("MAIL_MAILER");
-        // $auth_token = getenv("TWILIO_AUTH_TOKEN");
-        // $twilio_number = getenv("TWILIO_NUMBER");
-        // $client = new Client('AC0cf6b0a40260fea5cff0e51ab5c360f9', '79820c4f180276c8ffb4d7f55bd35c70');
-        // $message = $client->messages
-        // ->create("whatsapp:9173158283", // to
-        //          [
-        //              "from" => "whatsapp:+14155238886",
-        //              "body" => "Hello, there!"
-        //          ]
-        // );
 
-        // print($message->sid);
-
-
-        // $curl = curl_init();
-
-        // curl_setopt_array($curl, array(
-        //     CURLOPT_URL => 'https://api.interakt.ai/v1/public/message/',
-        //     CURLOPT_RETURNTRANSFER => true,
-        //     CURLOPT_ENCODING => '',
-        //     CURLOPT_MAXREDIRS => 10,
-        //     CURLOPT_TIMEOUT => 0,
-        //     CURLOPT_FOLLOWLOCATION => true,
-        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        //     CURLOPT_CUSTOMREQUEST => 'POST',
-        //     CURLOPT_POSTFIELDS => '{
-        //         "countryCode": "+91",
-        //         "phoneNumber": "9999999999",
-        //         "callbackData": "some text here",
-        //         "type": "Template",
-        //         "template": {
-        //             "name": "template_name_here",
-        //             "languageCode": "en",
-        //             "bodyValues": [
-        //                 "body_variable_value_1",
-        //                 "body_variable_value_n"
-        //             ]
-        //         }
-        //     }',
-        //     CURLOPT_HTTPHEADER => array(
-        //         'Authorization: Basic e50be13bf68adac9ed30e72b29206669c3c88b89',
-        //         'Content-Type: application/json'
-        //     ),
-        // ));
-
-        // $response = curl_exec($curl);
-
-        // curl_close($curl);
-        // echo $response;
     }
 
     public function otpverify(Request $request)
     {
 
-        $request->validate(['whatsapp_no' => 'required', 'otp' => 'required']);
+        $validator = Validator::make($request->all(), [
+            'whatsapp_no' => 'required',
+            'otp' => 'required'
+
+        ],[
+            'whatsapp_no.required' => 'The Whatsapp No is required',
+            'otp.required' => 'The OTP is required',
+        ]);
+        // Return errors if validation error occur.
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            // return response()->json(['status' => '0', 'message' => $validator->messages()->first()]);
+            return response()->json([
+                'message' => $validator->messages()->first()
+            ], 200);
+        }
+
 
         $otp = OTP::where('whatsapp_no', $request->whatsapp_no)->where('otp', $request->otp)->where('status', 'Active')->first();
         if ($otp) {
@@ -181,6 +167,8 @@ class AuthController extends Controller
             'business_name.max' => 'The Business name can not more than25 character',
             'business_name.unique' => 'The Business name is already been taken'
         ]);
+
+
         $user = User::findOrFail($id);
         $user->name = $request->full_name;
         $user->username = $request->business_name;
